dc82d56d5ed5574d2591c1bcb2ddcef592bba9d3
muskantomar990 <68536462+muskantomar990@users.noreply.github.com>
2020-08-12 22:01:28 +0530

    Add files via upload

---
+++ 1d array in c.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+ {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int i, n,sum=0;
+    scanf("%d", &n);
+//int a[n];
+
+    int *a = (int *)malloc(sizeof(int) * n);
+
+    for (int i = 0; i < n;i++)
+    {
+     scanf("%d", &a[i]);
+     sum = sum + a[i];
+    } 
+    printf("%d", sum);
+    return 0;
+}
